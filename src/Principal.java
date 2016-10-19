8e557d8f43ce27292865784e8814e3f4495d96bc
Trainerpl135 <Trainerpl135@SLFRPL135PCFORM.ensino.atec.local>
2016-10-19 12:03:34 +0100

    aula 0

---
+++ src/Principal.java
@@ -0,0 +1,35 @@
+
+
+public class Principal {
+
+	public Principal() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub		
+		
+		String mensagem = "Olá turma, de mensagem";
+		
+		System.out.println();
+		
+		int agua 	= 40;
+		int luz 	= 20;
+		int gas		= 20;
+		int fibra	= 60;
+		
+		/*
+		agua 	= 40;
+		luz		= 20;
+		gas		= 20;
+		fibra	= 60;
+		*/
+		
+		int despesas = agua + luz + gas + fibra;
+		// TODO MOSTRAR NO ECRA O TOTAL DE DESPESAS
+		
+		System.out.println(despesas);
+		
+	}
+
+}
---
+++ src/dia191016/Exercicio1.java
@@ -0,0 +1,9 @@
+package dia191016;
+
+public class Exercicio1 {
+
+	public Exercicio1() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
